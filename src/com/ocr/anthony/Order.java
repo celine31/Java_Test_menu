5572dc81f654bcc57a502f3bed4bbcdebb419956
celine31 <dadou20231@gmail.com>
2018-08-31 15:26:28 +0200

    la réponse est soumis à une condition qui renvoie le libellé au lieu du numéro

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,21 +1,31 @@
 package com.ocr.anthony;
 
 public class Order {
-    /**
-     * Display all available menus in the restaurant.
-     */
-    public void displayAvailableMenu() {
-      System.out.println("Choix menu");
-      System.out.println("1 - poulet");
-      System.out.println("2 - boeuf");
-      System.out.println("3 - végétarien");
-      System.out.println("Que souhaitez-vous commander");
+  /**
+   * Display all available menus in the restaurant.
+   */
+  public void displayAvailableMenu() {
+    System.out.println("Choix menu");
+    System.out.println("1 - poulet");
+    System.out.println("2 - boeuf");
+    System.out.println("3 - végétarien");
+    System.out.println("Que souhaitez-vous commander");
+  }
+
+  /**
+   * Display a selected menu.
+   *
+   * @param nbMenu The selected menu.
+   */
+  public void displaySelectedMenu(int nbMenu) {
+    if (nbMenu == 1) {
+      System.out.println("Vous avez choisi comme menu : poulet");
+    } else if (nbMenu == 2) {
+      System.out.println("Vous avez choisi comme menu : boeuf");
+    } else if (nbMenu == 3) {
+      System.out.println("Vous avez choisi comme menu : végétarien");
+    } else {
+      System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
     }
-    /**
-     * Display a selected menu.
-     * @param nbMenu The selected menu.
-     */
-    public void displaySelectedMenu(int nbMenu) {
-      System.out.println("Vous avez choisi le menu "+nbMenu);
-    }
+   }
 }
--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -12,28 +12,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
+  @BeforeEach
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+  }
 
-    @AfterEach
-    public void restoreStreams() {
-        System.setOut(System.out);
-    }
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(System.out);
+  }
 
-    Order order = new Order();
+  Order order = new Order();
 
-    @Test
-    public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
-        order.displayAvailableMenu();
-        assertEquals(false, outContent.toString().isEmpty());
-    }
-    @Test
-    public void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
-        order.displaySelectedMenu(3);
-        assertEquals("Vous avez choisi le menu 3\n", outContent.toString().replace("\r\n", "\n"));
-    }
+  @Test
+  public void Given_Chicken_When_DisplayMenuSelected_Then_DisplayChickenSentence() {
+    order.displaySelectedMenu(1);
+    assertEquals("Vous avez choisi comme menu : poulet\n", outContent.toString().replace("\r\n", "\n"));
+  }
+
+  @Test
+  public void Given_Beef_When_DisplayMenuSelected_Then_DisplayBeefSentence() {
+    order.displaySelectedMenu(2);
+    assertEquals("Vous avez choisi comme menu : boeuf\n", outContent.toString().replace("\r\n", "\n"));
+  }
+
+  @Test
+  public void Given_Vegetarian_When_DisplayMenuSelected_Then_DisplayVegetarianSentence() {
+    order.displaySelectedMenu(3);
+    assertEquals("Vous avez choisi comme menu : végétarien\n", outContent.toString().replace("\r\n", "\n"));
+  }
+
+  @Test
+  public void Given_TooBigValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+    order.displaySelectedMenu(15);
+    assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+  }
+
+  @Test
+  public void Given_NegativeValue_When_DisplayMenuSelected_Then_DisplayErrorSentence() {
+    order.displaySelectedMenu(-6);
+    assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
+  }
 }
